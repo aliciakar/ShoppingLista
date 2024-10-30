9dba9dc66d9890c56bf1167ff2ac4bb787f101de
Alicia K <60913@gymnasium.ax>
2024-10-30 10:10:46 +0200

    syvaduigbdho

--- ShoppingLista/settings.py
+++ ShoppingLista/settings.py
@@ -119,9 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'listor', 'static'),)
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
