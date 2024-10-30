bd6ca5152840a604bad4451cebe8d651ba34803f
Alicia K <60913@gymnasium.ax>
2024-10-30 08:15:57 +0200

    first commit

---
+++ listor/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class UserRegisterForm(UserCreationForm):
+    email=forms.EmailField()
+
+    class Meta:
+        model=User
+        fields=['username','email','password1','password2']
+
+    def clean_email(self):
+        email=self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('E-post-adressen är redan registrerad.')
+        return email
